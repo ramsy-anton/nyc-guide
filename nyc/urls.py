cdd0056ece1405cc268de1250d81a73ab59868a9
Ramsy Anton <anton.ra@hotmail.com>
2021-11-06 12:04:44 -0700

    Updated Views and Added Activity template

--- nyc/urls.py
+++ nyc/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.city, name='city'),
     path('<str:borough>', views.borough, name='borough'),
     path('<str:borough>/<str:activity>', views.activity, name='activity'),
-    path('<str:borough>/<str:activity>/<str:venue>', views.venue, name='venue')
+    path('<str:borough>/<str:activity>/<str:venue>', views.venue, name='venue'),
 ]
--- nyc/views.py
+++ nyc/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from django.shortcuts import render
 from .boroughs import boroughs
 
@@ -10,8 +11,11 @@ def borough(request, borough):
         return render(request=request, template_name='borough.html', context={ 'borough': borough, 'activities': boroughs[borough].keys() })
 
 # Create your views here.
-def activity():
-    pass
+def activity(request, borough, activity):
+    if request.method == 'GET':
+        return render(request=request, template_name='activity.html', context={ 'borough': borough, 'activity': activity, 'venues': boroughs[borough][activity]})
 
-def venue():
-    pass
+def venue(request, borough, activity, venue):
+    if request.method == 'GET':
+        return render(request=request, template_name='venue.html', context={
+        'borough': borough, 'activity': activity, 'venue': boroughs[borough][activity][venue]})
